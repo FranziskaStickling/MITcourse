4be5c9fd1373a7ea594a108158409ab07e519d5a
FranziskaStickling <franziska.stickling@tum.de>
2024-07-15 07:29:22 -0400

    lecture 1 and 2

--- MITlecture1.py
+++ MITlecture1.py
@@ -1,2 +1,16 @@
-print('Hello World')
-print('Hallo')
+import numpy
+
+# input a number x
+x = input("Enter a number x:")
+# Convert input to integer 
+x = int(x)
+
+# input a number y
+y = input("Enter a number y:")
+# Convert input to integer 
+y = int(y)
+
+print(x**y)
+
+print(numpy.log2(x))
+
---
+++ MITlecture2.py
@@ -0,0 +1,9 @@
+'''while input("Choose right or left: ") == "right":
+    input("Choose right or left: ")
+print("you got out")'''
+
+
+mysum = 0 
+for i in range(7,10):
+	mysum += i
+print(mysum)
